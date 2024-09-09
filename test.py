503e9cba1f962db356e5f101772034dd8c416e15
horakL88 <154601552+horakL88@users.noreply.github.com>
2024-09-09 18:11:55 +0200

    Delete test.py

--- test.py
+++
@@ -1,6 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = requests.get("https://www.raiffeisen.hu/hasznos/arfolyamok/vallalati/valutaarfolyamok?p_p_id=hu_raiffeisen_website_currency_rates_web_portlet_CurrencyRatesPortlet_INSTANCE_VyVoydIU31cU&p_p_lifecycle=0&p_p_state=normal&p_p_mode=view").text
-
-soup = BeautifulSoup(url)
